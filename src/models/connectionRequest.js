1a6867fcd338067592658ba6bfaf8d74929f0443
Siftain Ejaz <siftainejaz@gmail.com>
2025-07-08 13:05:06 +0530

    Implemented feed API and pagination

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -24,7 +24,7 @@ const connectionRequestSchema = new mongoose.Schema(
 {timestamps : true}
 );
 
-connectionRequestSchema.index({fromUserId : 1, toUserId : 1});
+connectionRequestSchema.index({fromUserId : 1, toUserId : 1}); //Compound Indexing
 
 connectionRequestSchema.pre("save",function(next){
     const loggedInUser = this;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const {userAuth} = require("../middlewares/auth.js");
 const ConnectionRequest = require("../models/connectionRequest.js");
+const User = require("../models/user.js");
 
 //Get all pending connection requests for the loggedIn USer
 userRouter.get("/user/requests/received", userAuth , async (req,res) => {
@@ -10,10 +11,10 @@ userRouter.get("/user/requests/received", userAuth , async (req,res) => {
    {
         const loggedInUser = req.user;
         //In the CR DB, toUserId = loggedinUser._id and status = interested
-
+        const USER_DISPLAY_DATA = "firstName lastName age gender about photoUrl skills";
         const connectionRequests = await ConnectionRequest.find(
             {toUserId : loggedInUser._id , 
-             status : "interested"}).populate("fromUserId" , "firstName lastName age gender about photoUrl");
+             status : "interested"}).select("fromUserId").populate("fromUserId" ,USER_DISPLAY_DATA);
            // populate("fromUserId", ["firstName","lastName"]);
         //Find all the documents that matches the Id
         if(connectionRequests.length==0)
@@ -71,6 +72,53 @@ userRouter.get("/user/connections", userAuth , async (req,res) => {
         res.json({message : `${matchedConnections.length} connection(s) found!`,
                  data : connections});
     }
+    catch(err)
+    {
+        res.status(400).send("ERROR : " +err.message);
+    }
+})
+
+userRouter.get("/user/feed" , userAuth , async (req,res) => {
+
+    try
+    {
+        const loggedInUser = req.user;
+        const page = req.query.page || 1;
+        let limit = req.query.limit || 10;
+        limit = limit > 50? 10 : limit;
+        const skip = (page-1) * limit;
+
+        //Should display all user except
+        //own profile
+
+        const USER_DISPLAY_DATA = "firstName lastName age gender about photoUrl skills";
+
+
+        //connection request is sent + received by the user
+        const connectionRequests = await ConnectionRequest.find(
+            {$or : [ 
+                {fromUserId : loggedInUser._id},
+                {toUserId : loggedInUser._id}
+            ]
+            }).select("fromUserId toUserId");
+
+        //Get all the unique entries
+        const hiddenUserFromFeed = new Set();
+        connectionRequests.forEach(connection => {
+            hiddenUserFromFeed.add(connection.fromUserId.toString());
+            hiddenUserFromFeed.add(connection.toUserId.toString());
+        });
+
+        //Excludes the (sent+received) request for loggedInUser and exclude self
+        const toDisplayUsers = await User.find({
+            $and : [
+                {_id : {$nin :  Array.from(hiddenUserFromFeed)}},
+                {_id : { $ne : loggedInUser._id}}
+            ]}).select(USER_DISPLAY_DATA).skip(skip).limit(limit);
+    
+        res.json({data : toDisplayUsers});
+    }
+
     catch(err)
     {
         res.status(400).send("ERROR : " +err.message);
